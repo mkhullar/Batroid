7ef9bb33cf6c510103da3c6e32f4a41b54384f34
murlee417 <murlee417@gmail.com>
2016-11-16 00:42:00 -0700

    Update Birthdays.java

--- app/src/main/java/mcgroup10/com/batroid/Birthdays.java
+++ app/src/main/java/mcgroup10/com/batroid/Birthdays.java
@@ -1,19 +1,206 @@
 package mcgroup10.com.batroid;
 
-import android.os.Bundle;
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by abhishekzambre on 13/11/16.
  */
 
 public class Birthdays extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
+    Button sendBtn;
+    EditText txtphoneNo;
+    EditText txtMessage;
+    String phoneNo;
+    String message;
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100; // Request code for READ_CONTACTS. It can be any number > 0.
+    private static final String TAG = "MainActivity";
+    ArrayList<String> contactName = new ArrayList<String>();
+    ArrayList<String> contactNumber = new ArrayList<String>();
+    TextView textDetail;
+    StringBuffer sb = new StringBuffer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_birthdays);
 
+        textDetail = (TextView) findViewById(R.id.textView1);
+
+        sendBtn = (Button) findViewById(R.id.btnSendSMS);
+
+
+        showContacts(); // Read and show the contacts
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendSMSMessage();
+            }
+        });
+    }
+
+    private void showContacts() {
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+        } else {
+            // Android version is lesser than 6.0 or the permission is already granted.
+            getContactNames();
+        }
+    }
+
+    protected void sendSMSMessage() {
+        //phoneNo = txtphoneNo.getText().toString();
+        message = "Happy birthday!";
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    for (int i = 0; i < contactNumber.size(); i++) {
+                        smsManager.sendTextMessage(contactNumber.get(i), null, message, null, null);
+                        Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "SMS failed, please try again.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                break;
+            }
+            case PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showContacts(); // Permission is granted
+                } else {
+                    Toast.makeText(this, "Until you grant the permission, we canot display the names", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+
+    }
+
+    /*
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showContacts(); // Permission is granted
+            } else {
+                Toast.makeText(this, "Until you grant the permission, we canot display the names", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    */
+
+    private Cursor getContactsBirthdays() {
+        Uri uri = ContactsContract.Data.CONTENT_URI;
+        String[] projection = new String[]{
+                ContactsContract.Contacts.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Event.CONTACT_ID,
+                ContactsContract.CommonDataKinds.Event.START_DATE
+        };
+        String where =
+                ContactsContract.Data.MIMETYPE + "= ? AND " +
+                        ContactsContract.CommonDataKinds.Event.TYPE + "=" +
+                        ContactsContract.CommonDataKinds.Event.TYPE_BIRTHDAY;
+
+        String[] selectionArgs = new String[]{
+                ContactsContract.CommonDataKinds.Event.CONTENT_ITEM_TYPE
+        };
+        String sortOrder = null;
+        ContentResolver cr = getContentResolver();
+        return cr.query(uri, projection, where, selectionArgs, sortOrder);
+        //return managedQuery
+    }
+
+    private void getContactNames() {
+        Cursor cursor = getContactsBirthdays();
+        int bDayColumn = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Event.START_DATE);
+        int nameColumn = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+        String number;
+        while (cursor.moveToNext()) {
+            String bDay = cursor.getString(bDayColumn);
+            String name = cursor.getString(nameColumn);
+
+
+            Date date = new Date();
+            String modifiedDate = new SimpleDateFormat("yyyy-MM-dd").format(date);
+            //Log.d(TAG, "today's date: " + modifiedDate);
+
+            if (modifiedDate.equals(bDay)) {
+                number = getPhoneNumber(this, name);
+                Log.d(TAG, "Name: " + name);
+                Log.d(TAG, "Birthday: " + bDay);
+                Log.d(TAG, "Number: " + number);
+                Log.i(TAG, "........................");
+                contactName.add(name);
+                contactNumber.add(number);
+            }
+        }
+        Log.i(TAG, "*************************");
+        Log.d(TAG, "contactName: " + contactName);
+        Log.d(TAG, "contactNumber: " + contactNumber);
+        Log.i(TAG, "*************************");
+        sb.append("~~~Today's Birthday~~~ \n");
+        for (int i = 0; i < contactName.size(); i++) {
+            sb.append("\n" + contactName.get(i) + "\n" + contactNumber.get(i) + "\n");
+        }
+        textDetail.setText(sb);
+    }
+
+    public String getPhoneNumber(Context context, String name) {
+        String ret = null;
+        String selection = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " like'%" + name + "%'";
+        String[] projection = new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER};
+        Cursor c = context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                projection, selection, null, null);
+        if (c.moveToFirst()) {
+            ret = c.getString(0);
+        }
+        c.close();
+        if (ret == null)
+            ret = "Unsaved";
+        return ret;
     }
 }
