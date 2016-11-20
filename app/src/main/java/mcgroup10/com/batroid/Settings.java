b766fd06deb5f35578373c36e77d487aac2f33a1
abhishekzambre <abhishek.zambre@gmail.com>
2016-11-19 17:37:46 -0700

    Adding settings layout and menu

---
+++ app/src/main/java/mcgroup10/com/batroid/Settings.java
@@ -0,0 +1,17 @@
+package mcgroup10.com.batroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by abhishekzambre on 19/11/16.
+ */
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.action_settings);
+    }
+}
